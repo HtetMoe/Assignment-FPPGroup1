41ce439c1a2b0e9d7e2e4463497bc82e6f4a302e
htetmoephyu <hmp.ucsm@gmail.com>
2024-05-28 23:04:57 -0500

    refactor code

--- src/Prog1.java
+++ src/Prog1.java
@@ -2,13 +2,17 @@ import java.util.Random;
 
 public class Prog1 {
     public static void main(String[] args) {
-        Random rand =new Random();
-        int x = rand.nextInt(1,9);
-        int y = rand.nextInt(3,14);
-        double r1 = Math.pow(Math.PI, x);
-        double r2 = Math.pow(y, Math.PI);
+        Random rand = new Random();
 
-        System.out.printf("πx = %.2f%n", r1 );
-        System.out.printf("yπ = %.2f%n", r2);
+        int x = rand.nextInt(1, 9);
+        int y = rand.nextInt(3, 14);
+
+        double pi = Math.PI;
+        double result1 = Math.pow(pi, x); // π power x
+        double result2 = Math.pow(y, pi);// y power π
+
+        //printout with string template
+        System.out.printf("π power x = %.2f%n", result1);
+        System.out.printf("y power π = %.2f%n", result2);
     }
 }
