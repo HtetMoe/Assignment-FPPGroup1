0fc04f61ae304b2c07f093deb920114dd3992725
htetmoephyu <hmp.ucsm@gmail.com>
2024-05-29 22:46:25 -0500

    modify problem 3

--- src/assignment2/problem3/Event.java
+++ src/assignment2/problem3/Event.java
@@ -1,18 +1,16 @@
-package problem2;
+package assignment2.problem3;
 
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
-
-
-public class event {
+public class Event {
     private String name;
     private LocalDate date;
     private LocalTime time;
     private LocalDateTime dateTime;
     private ZoneId zone;
 
-    event(String name, LocalDate date, LocalTime time, ZoneId zone) {
+    public Event(String name, LocalDate date, LocalTime time, ZoneId zone) {
         this.name = name;
         this.date = date;
         this.time = time;
@@ -21,28 +19,31 @@ public class event {
     }
 
     public void checkEventTime() {
-        if(date.isLeapYear())
-            System.out.println( date.getYear() + " is a leap year.");
+        if (date.isLeapYear())
+            System.out.println(date.getYear() + " is a leap year.");
         else
-            System.out.println( date.getYear() + " is not a leap year.");
+            System.out.println(date.getYear() + " is not a leap year.");
+
         long days = ChronoUnit.DAYS.between(LocalDate.now(), date);
         System.out.println("The event will start on " + date.getDayOfWeek() + ", with " + days + " days left.");
     }
+
     @Override
     public String toString() {
-        return date.getDayOfWeek() + ", "  + date.getMonth() + " " + date.getDayOfMonth() + ", " + date.getYear() + " @ " + time + " [" + ZoneId.systemDefault() + "]";
+        return STR."\{date.getDayOfWeek()}, \{date.getMonth()} \{date.getDayOfMonth()}, \{date.getYear()} @ \{time} [\{ZoneId.systemDefault()}]";
     }
 
-    public void showEventDate(){
-        System.out.println(date.getDayOfWeek() + ", "  + date.getMonth() + " " + date.getDayOfMonth() + ", " + date.getYear() + " @ " + time + " [" + ZoneId.systemDefault() + "]");
+    public void showEventDate() {
+        System.out.println(STR."\{date.getDayOfWeek()}, \{date.getMonth()} \{date.getDayOfMonth()}, \{date.getYear()} @ \{time} [\{ZoneId.systemDefault()}]");
     }
 
-    public void showEventDateTimeInSpecificTimeZone(){
+    public void showEventDateTimeInSpecificTimeZone() {
         ZonedDateTime originalZoneDateTime = dateTime.atZone(ZoneId.systemDefault());
         ZonedDateTime newZoneDateTime = originalZoneDateTime.withZoneSameInstant(zone);
+
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm MM/dd/yyyy");
         String formattedDateTime = formatter.format(newZoneDateTime);
-        System.out.println("Event will start at " + formattedDateTime + " in " + zone + " time zone");
+        System.out.println(STR."Event will start at \{formattedDateTime} in \{zone} time zone");
     }
 
 }
