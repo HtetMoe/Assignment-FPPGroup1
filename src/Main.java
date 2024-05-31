f9d148d2a8eb08c397db5a2d3bc977f2631f55f0
huazhou <hua.zhou@miu.edu>
2024-05-31 15:25:52 -0500

    sync local to remote

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,34 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        int n = 5;
+        for(int i = 0; i < n; ++i){
+            for(int j = 0; j < n; ++j){
+                System.out.printf("%-3s","*");
+            }
+            System.out.println();
+        }
+        String s = reverseString("hello");
+        System.out.println(s);
+        int sum = sum(5);
+        System.out.println(sum);
     }
+
+    public static String reverseString(String s){
+        if (s.length() == 1)
+            return s;
+        String n = reverseString(s.substring(1));
+        return n + s.charAt(0);
+    }
+
+    public static int sum(int n){
+        if (n == 0)
+            return 0;
+        else
+            return n + sum(n - 1);
+    }
+
 }
--- src/problem3/event.java
+++ src/problem3/event.java
@@ -6,6 +6,7 @@ import java.time.temporal.ChronoUnit;
 
 
 public class event {
+    int num;
     private String name;
     private LocalDate date;
     private LocalTime time;
