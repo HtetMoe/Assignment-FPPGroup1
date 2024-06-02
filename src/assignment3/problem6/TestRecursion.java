dbfda5011ded913a1f08bb7b82990be4d76b04b9
huazhou <hua.zhou@miu.edu>
2024-06-02 15:00:31 -0500

    add solution for problem 5 add test cases for problem 5 and problem 4

--- src/assignment3/problem6/TestRecursion.java
+++ src/assignment3/problem6/TestRecursion.java
@@ -2,9 +2,12 @@ package assignment3.problem6;
 
 import assignment3.problem1.Exponential;
 import assignment3.problem2.MinimumCharacter;
+import assignment3.problem4.IsPalindrome;
+import assignment3.problem5.FindMax;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestRecursion {
     @Test
@@ -18,5 +21,20 @@ public class TestRecursion {
         char act = MinimumCharacter.minChar("jkwlqau", 0);
         char exp = 'a';
         assertEquals(exp, act);
+    }
+
+    @Test
+    void Test4 () {
+        boolean act = IsPalindrome.isPalindrome(12321   );
+        assertTrue(act);
+
+    }
+
+    @Test
+    void Test5 () {
+        int [] a = {23,-9,89,1,0,33, 100, 56, -90};
+        int act = FindMax.findMax(a);
+        int exp = 100;
+        assertEquals(exp,act);
     }
 }
