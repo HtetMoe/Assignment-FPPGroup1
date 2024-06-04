6a9b7c1b7c1207d78cd72226bf646ffe74d7748c
huazhou <hua.zhou@miu.edu>
2024-06-03 21:30:39 -0500

    change commissionRate in %change commissionRate in %change commissionRate in %change commissionRate in %change commissionRate in %change commissionRate in %change commissionRate in %change commissionRate in %change commissionRate in %

--- src/assignment4/problem4/BasePlusCommisionEmployee.java
+++ src/assignment4/problem4/BasePlusCommisionEmployee.java
@@ -16,19 +16,19 @@ public final class BasePlusCommisionEmployee extends CommissionEmployee{
     }
 
     public double getPayment(){
-        return baseSalary + super.getGrossSales() * super.getCommissionRate();
+        return baseSalary + super.getGrossSales() * super.getCommissionRate()/100;
     }
 
     @Override
     public String toString() {
         return String.format("""
-                Firstname: %s,
-                Lastname: %s,
-                SSN: %s,
-                GrossSale: %.2f;
-                CommissionRate: %.2f;
-                BaseSalary: %.2f;
-                Payment: %.2f;
+                Firstname: %s
+                Lastname: %s
+                SSN: %s
+                GrossSale: %.2f
+                CommissionRate: %.2f%%
+                BaseSalary: %.2f
+                Payment: %.2f
                 """,
                 super.getFirstName(),
                 super.getLastName(),
