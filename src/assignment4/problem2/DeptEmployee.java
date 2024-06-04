05a9cd0fc1b864e4afd9c928584c76acf2dd8d48
huazhou <hua.zhou@miu.edu>
2024-06-03 20:11:35 -0500

    code for problem2

--- src/assignment4/problem2/DeptEmployee.java
+++ src/assignment4/problem2/DeptEmployee.java
@@ -1,2 +1,35 @@
-package assignment4.problem2;public class DeptEmployee {
+package assignment4.problem2;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DeptEmployee {
+    protected String name;
+    protected LocalDate hireDate;
+    protected final double salary;
+    public DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+
+    DeptEmployee(double salary){
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getHireDate() {
+        return hireDate;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHireDate(LocalDate hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public double computeSalary(){
+        return salary;
+    }
 }
